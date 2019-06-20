554b9edc5e05bcf21c67a8d6d9261bea5057078c
Nikolai <dvininnick@gmail.com>
2019-06-20 17:49:02 +0300

    HW02 - Implement InMemoryUserRepositoryImpl

--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -6,7 +6,7 @@ import java.util.Set;
 
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
-public class User extends AbstractNamedEntity {
+public class User extends AbstractNamedEntity implements Comparable<User> {
 
     private String email;
 
@@ -75,6 +75,11 @@ public class User extends AbstractNamedEntity {
 
     public String getPassword() {
         return password;
+    }
+
+    @Override
+    public int compareTo(User user) {
+        return this.name.compareTo(user.name);
     }
 
     @Override
