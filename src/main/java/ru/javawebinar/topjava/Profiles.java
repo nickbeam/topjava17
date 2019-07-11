c365d9f2d629a4e949070356e5e8533d1997a369
Nikolai <dvininnick@gmail.com>
2019-07-11 17:51:37 +0300

    HW05 - Tasks 1 - 4, 6 complete

--- src/main/java/ru/javawebinar/topjava/Profiles.java
+++ src/main/java/ru/javawebinar/topjava/Profiles.java
@@ -15,12 +15,12 @@ public class Profiles {
     //  Get DB profile depending of DB driver in classpath
     public static String getActiveDbProfile() {
         try {
-            Class.forName("org.postgresql.Driver");
-            return POSTGRES_DB;
+            Class.forName("org.hsqldb.jdbcDriver");
+            return Profiles.HSQL_DB;
         } catch (ClassNotFoundException ex) {
             try {
-                Class.forName("org.hsqldb.jdbcDriver");
-                return Profiles.HSQL_DB;
+                Class.forName("org.postgresql.Driver");
+                return POSTGRES_DB;
             } catch (ClassNotFoundException e) {
                 throw new IllegalStateException("Could not find DB driver");
             }
